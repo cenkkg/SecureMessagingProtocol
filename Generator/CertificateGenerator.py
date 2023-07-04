d1fbf8cc8f6a085303a797f673a50afde2bba008
cenkgurbet <cenk.gurbet@deepnetwork.com>
2023-07-04 21:14:00 +0200

    CG - Decouple methods in SaveOrganisationToSystem and CertificateGenerator - 1

--- Generator/CertificateGenerator.py
+++ Generator/CertificateGenerator.py
@@ -5,9 +5,7 @@ from cryptography.hazmat.primitives import hashes
 from Generator.SavePrivateKeyAndCertificate import savePrivateKeyAndCertificate
 
 
-def generateCertificate(certificateSubject):
-
-    private_key = generatePrivateKey()
+def generateCertificate(certificateSubject, privateKey):
 
     subject = x509.Name([
         x509.NameAttribute(x509.NameOID.COMMON_NAME, certificateSubject),
@@ -19,14 +17,13 @@ def generateCertificate(certificateSubject):
     ).issuer_name(
         subject
     ).public_key(
-        private_key.public_key()
+        privateKey.public_key()
     ).serial_number(
         x509.random_serial_number()
     ).not_valid_before(
         datetime.datetime.utcnow()
     ).not_valid_after(
         datetime.datetime.utcnow() + datetime.timedelta(days=365)
-    ).sign(private_key, hashes.SHA256())
+    ).sign(privateKey, hashes.SHA256())
 
-
-    savePrivateKeyAndCertificate(private_key, cert, certificateSubject)
+    return cert
--- Generator/SaveOrganisationToSystem.py
+++ Generator/SaveOrganisationToSystem.py
@@ -1,10 +1,13 @@
 from PrivateKeyGenerator import generatePrivateKey
 from CertificateGenerator import generateCertificate
+from SavePrivateKeyAndCertificate import savePrivateKeyAndCertificate
 
 def saveOrganisationToSystem(organisationID):
     # Sign-up into database.
     try:
-        generateCertificate(organisationID)
+        newPrivateKey = generatePrivateKey()
+        newCertificate = generateCertificate(organisationID, newPrivateKey)
+        savePrivateKeyAndCertificate(newPrivateKey, newCertificate, organisationID)
 
     except Error as e:
-        print("Error during saving certificates.")
+        print("Error during creting certificate or private key.")
